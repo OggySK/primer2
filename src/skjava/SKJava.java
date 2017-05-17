b6fce0f465798d7b8b70ba8a53b2b4dbcb61e3fc
ognjen popovic <ognjen.popovic@infrakit.com>
2017-05-17 22:05:24 +0200

    prvi komit

---
+++ src/skjava/SKJava.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skjava;
+
+/**
+ *
+ * @author ogypo
+ */
+public class SKJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String ime = "Svet kompjutera";
+        int tiraz = 100000;
+        boolean najboljiCasopis = true;
+        int[] oceneIgara = {88, 83, 40, 74, 91};
+        double pi = 3.14;
+
+        System.out.println("pi: " + pi);
+        System.out.println("prva ocena: " + oceneIgara[0]);
+    }
+}
